2f34fb45141af12e3d6412bdc495bd25751120cc
Zung VQ <dungvuquang110@gmail.com>
2024-10-17 21:59:02 +0700

    vid3

---
+++ video3/video3.js
@@ -0,0 +1,10 @@
+console.log(`Hello "''" :v`); //su dung backtick
+
+const x=5
+//x=6   //se bi loi
+console.log(x);
+
+let a=5, b=5, c=6;
+console.log("Test string", "a =", a, 'b =', b, 'c =', c)
+console.log("Test string a = " + a +' b = ' + b + ' c = ' + c)
+
