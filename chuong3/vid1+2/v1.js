eaa19f71fa245470b51cfee7a8ebd7175eb59f6a
Zung VQ <dungvuquang110@gmail.com>
2024-11-05 13:09:25 +0700

    c3v1+2

---
+++ chuong3/vid1+2/v1.js
@@ -0,0 +1,16 @@
+console.log('hola');
+
+let arrTop4 = ['MU', 'MC', 'Liver', 'Chel'];
+
+//for loops
+for(let i=0; i<arrTop4.length; i++){
+    console.log('Top', i + 1, arrTop4[i]);
+}
+
+//while loops
+let j = 0;
+while(j < arrTop4.length){
+    console.log('Top', j + 1, arrTop4[j]);
+    j++;
+}
+
