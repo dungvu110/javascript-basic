fbed15e35c90c27ca7ed02acf12ef651a66cf0d3
Zung VQ <dungvuquang110@gmail.com>
2024-11-05 09:42:55 +0700

    vid9

---
+++ chuong1/vid9/v9.js
@@ -0,0 +1,18 @@
+//array
+console.log(`hello from js`)
+
+let a = {
+    name: 'dung',
+    nationality: 'Vietnamese'
+}
+
+let b = ['chao', 'hello', 'privyet', 'nihao'];
+console.log('type of b:', typeof b)
+console.log(b)
+
+let friends = [
+    {name: 'dung', nationality: 'Vietnamese'},
+    {name: 'john', nationality: 'British'},
+    {name: 'alek', nationality: 'Russian'}
+]
+console.log('me and the friends:', friends)
