b2883d3ed9acaeb7ad242f8eba9d86c8b73ba29d
Zung VQ <dungvuquang110@gmail.com>
2024-11-05 10:38:43 +0700

    vid10

---
+++ chuong2/vid10/v10.js
@@ -0,0 +1,20 @@
+console.log('privyet iz js')
+
+//So Sánh, ==, ===
+let a = 5;
+let b = '5';
+
+console.log('compare a,b: ', 'a=', a, 'b=', b, 'result:', a==b)     //true
+console.log('compare a,b: ', 'a=', a, 'b=', b, 'result:', a===b)    //false
+
+
+//Phân Biệt Empty, NULL, Undefined
+//empty '', "", [], {}
+let c = ''
+console.log('check value', c)
+
+let d
+console.log('check value', d)   //undefined
+
+let e = null;
+console.log('check value', e)
