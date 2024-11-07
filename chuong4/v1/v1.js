dfb1a70d56b339ad3a397ff35c224a1bbc2db359
Zung VQ <dungvuquang110@gmail.com>
2024-11-07 07:37:20 +0700

    c4v1

---
+++ chuong4/v1/v1.js
@@ -0,0 +1,14 @@
+console.log('приветь')
+
+//declare function
+
+//input parameter(s)
+function sum(a, b){
+    // return;    //undefined
+    return a + b;
+}
+
+console.log('sum a + b =', sum(4, 5));
+
+
+
