d16d3c4f8f6a88ad601add88cec5ddc8af46e8c3
Zung VQ <dungvuquang110@gmail.com>
2024-11-07 07:58:50 +0700

    c4v3

---
+++ chuong4/v3/v3.js
@@ -0,0 +1,15 @@
+console.log('hello')
+
+//function
+function sum(a, b){
+    return a + b;
+}
+
+console.log('sum functions, a + b =', sum(4, 5))
+
+//arrow functions
+let sum_arr = (a, b) => {
+    return a + b;
+}
+console.log('sum arr function, a + b =', sum_arr(4, 5))
+
